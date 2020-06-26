6b29cf0cb44f08d6517ae68c73cd79a51cdd376d
Noe Leiva <leiva.noe.86@gmail.com>
2020-06-26 07:58:58 -0500

    initial letter construtor definition

---
+++ letter.js
@@ -0,0 +1,12 @@
+
+
+function Letter(){
+    this.userLetter = ""; 
+    this.letterGuessed = false;
+    this.returnCharacter = function(){
+
+    }
+    this.userGuess = function(letter){
+        
+    }
+}
